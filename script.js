8278528aa726becf4540401f4bab3e6ef3915549
CarlosHernandezGonzalez <carlos.hgonzalez96@hotmail.com>
2018-10-25 11:08:14 +0200

    'javascript'

---
+++ script.js
@@ -0,0 +1,10 @@
+var nombre="Carlos";
+console.log('el Nombre es:',nombre);
+var apellido="Hernandez";
+console.log('el apellido es:',apellido);
+var nombre_completo=nombre+""+apellido;
+
+console.log('el Nombre completo es:',nombre_completo);
+// cada coma que se ha puesto, indica una variable a mostrar
+// El console.log escupe el resultado de lo que hay dentro de la console
+// cada vez que quiera saber el valor de cada variable tendre que usar un console log para ver como van variando
